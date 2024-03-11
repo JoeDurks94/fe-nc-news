48b05fcf9660fff9c0f8cc9daede1f2806c018d6
Joe Durkan <durkanjm01@hotmail.co.uk>
2024-03-11 16:54:47 +0000

    task 4 complete

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -1,19 +1,20 @@
 import { React, useEffect, useState } from "react";
 import { getAllArticles } from "../utils/getAllArticles";
+import { Link } from "react-router-dom";
 
 const Articles = () => {
 	const [articles, setArticles] = useState({});
 	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
-		setIsLoading(true);
-		getAllArticles()
-			.then((data) => {
-				setArticles(data.articles);
-			})
-			.then(() => {
-				setIsLoading(false);
-			});
+		setIsLoading(true)
+        getAllArticles()
+                .then((data) => {
+                    setArticles(data.articles);
+                })
+                .then(() => {
+                    setIsLoading(false);
+                });
 	}, []);
 
 	return isLoading ? (
@@ -23,11 +24,13 @@ const Articles = () => {
 			<ul className="article-list">
 				{articles.map((article) => {
 					return (
-						<div className="article-item">
-							<img src={article.article_img_url} />
-							<h3>{article.title}</h3>
-                            <p>Votes: {article.votes}</p>
-						</div>
+						<Link to ={`/articles/${article.article_id}`}>
+                            <div className="article-item">
+                                <img className="img-article-list" src={article.article_img_url} />
+                                <h3>{article.title}</h3>
+                                <p>Votes: {article.votes}</p>
+                            </div>
+                        </Link>
 					);
 				})}
 			</ul>
